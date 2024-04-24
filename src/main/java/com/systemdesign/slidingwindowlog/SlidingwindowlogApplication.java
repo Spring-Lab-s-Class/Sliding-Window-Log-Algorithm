882c58a6def6ad6d239fafac11157148bde59218
jay-so <merry9504@naver.com>
2024-04-24 23:54:43 +0900

    Init: 프로젝트 초기 세팅

---
+++ src/main/java/com/systemdesign/slidingwindowlog/SlidingwindowlogApplication.java
@@ -0,0 +1,13 @@
+package com.systemdesign.slidingwindowlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlidingwindowlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlidingwindowlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/systemdesign/slidingwindowlog/SlidingwindowlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.systemdesign.slidingwindowlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlidingwindowlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
